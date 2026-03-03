39ccdbe39ab0553360b13401ef8025985293fec0
anup <44515437+Anupkuppasagoudar@users.noreply.github.com>
2026-03-03 18:39:48 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    // Remove AppComponent from here
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppComponent // Move it here because Standalone components act like modules
   ],
   providers: [],
   bootstrap: [AppComponent]
