f8141f905373d2364e11ed379e014a49e4df8ddd
anup <44515437+Anupkuppasagoudar@users.noreply.github.com>
2026-03-03 17:00:11 +0530

    Create app.component.ts with basic Angular component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
 import { Component } from '@angular/core';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faInstagram, faFacebook, faWhatsapp, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faMapMarkerAlt, faStore } from '@fortawesome/free-solid-svg-icons';
-import { FloatingPopupComponent } from '../floating-popup/floating-popup.component';
-
 @Component({
   selector: 'app-root',
-  imports: [FontAwesomeModule,FloatingPopupComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sample1TPC';
-  faInstagram = faInstagram;
-  faFacebook = faFacebook;
-  faWhatsapp = faWhatsapp;
-  faYoutube = faYoutube;
-  faLinkedin = faLinkedin;
-  faMapMarkerAlt = faMapMarkerAlt;
-  faStore = faStore;
-  
+  title = 'my-angular-app';
 }
