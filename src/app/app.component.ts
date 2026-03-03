192b8a50935c5d33aaaa580eb979017738d0e3ab
anup <44515437+Anupkuppasagoudar@users.noreply.github.com>
2026-03-03 18:44:41 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// Import the specific icons your HTML is looking for
+import { 
+  faInstagram, 
+  faFacebook, 
+  faWhatsapp, 
+  faYoutube, 
+  faLinkedin 
+} from '@fortawesome/free-brands-svg-icons';
+import { faMapMarkerAlt, faStore } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
+  standalone: true, // This is key for Angular 19
+  imports: [
+    CommonModule, 
+    FontAwesomeModule
+    /* If you have a FloatingPopupComponent, import and add it here too */
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  title = 'tpcec';
+
+  // Define these so [icon]="faInstagram" in your HTML works
+  faInstagram = faInstagram;
+  faFacebook = faFacebook;
+  faWhatsapp = faWhatsapp;
+  faYoutube = faYoutube;
+  faLinkedin = faLinkedin;
+  faMapMarkerAlt = faMapMarkerAlt;
+  faStore = faStore;
 }
